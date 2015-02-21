69e77088438612078fc4e6cb6ae24cf1d3bf9640
tvand7093 <tylervanderhoef@TylersMacBookPro-2.local>
2015-02-21 12:59:35 -0800

    End tutorial

--- DataBinding/DataBinding/App.cs
+++ DataBinding/DataBinding/App.cs
@@ -10,6 +10,21 @@ namespace DataBinding
 		{
 			MainPage = new MainPage ();
 		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
 	}
 }
 
--- DataBinding/DataBinding/ViewModels/MainPageViewModel.cs
+++ DataBinding/DataBinding/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace DataBinding.ViewModels
 {
@@ -13,9 +15,36 @@ namespace DataBinding.ViewModels
 				OnPropertyChanged ("Message");
 			}
 		}
+
+		private string name;
+		public string Name {
+			get { return name; }
+			set {
+				name = value;
+				//notify the view that this has changed so it knows to refresh.
+				OnPropertyChanged ("Name");
+				//since the value changed, we need to also trigger the valid input
+				OnPropertyChanged("HasValidInput");
+			}
+		}
+
+		//the command to use for clicks
+		//notice we don't do the "OnPropertyChanged since
+		//the button command should never change.
+		public ICommand ShowNameCommand { get; private set;}
+
+		//this is used to determine if the button can be clicked.
+		public bool HasValidInput { get { return !String.IsNullOrEmpty (Name); } }
+
+
 		public MainPageViewModel ()
 		{
-			Message = "Hello world!";
+			//create the action that gets called on the button click
+			ShowNameCommand = new Command (() => {
+				//set the message to show the data typed into the text editor
+				//note: the Name property is now filling data from the UI!
+				Message = "Hello, my name is: " + Name;
+			});
 		}
 	}
 }
